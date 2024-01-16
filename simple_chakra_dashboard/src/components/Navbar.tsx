90d7e2a2e96c43d1ae8ba74c849449e3b7a0917b
Ellie Wang <axnw997@aucklanduni.ac.nz>
2024-01-17 11:50:25 +1300

    flex layout, colorScheme, HStack, Box, button etc

---
+++ simple_chakra_dashboard/src/components/Navbar.tsx
@@ -0,0 +1,30 @@
+import {
+  Box,
+  Button,
+  Flex,
+  HStack,
+  Heading,
+  Spacer,
+  Text,
+} from "@chakra-ui/react";
+
+const Navbar = () => {
+  return (
+    <Flex as="nav" p="10px" alignItems={"center"}>
+      <Heading as="h1" colorScheme="red">
+        Dojo Tasks
+      </Heading>
+      <Spacer />
+
+      <HStack spacing="20px">
+        <Box bg="gray.200" p="10px">
+          M
+        </Box>
+        <Text>mario@test.com</Text>
+        <Button colorScheme="purple">Logout</Button>
+      </HStack>
+    </Flex>
+  );
+};
+
+export default Navbar;
--- simple_chakra_dashboard/src/layouts/RootLayout.tsx
+++ simple_chakra_dashboard/src/layouts/RootLayout.tsx
@@ -1,8 +1,9 @@
 import { Outlet } from "react-router-dom";
-
+import Navbar from "../components/Navbar";
 export const RootLayout = () => {
   return (
     <div>
+      <Navbar />
       <Outlet />
     </div>
   );
