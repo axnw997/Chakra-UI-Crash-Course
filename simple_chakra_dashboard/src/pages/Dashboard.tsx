793f3d25997250980d8a2d8618a80ca05c955218
Ellie Wang <axnw997@aucklanduni.ac.nz>
2024-01-17 18:48:43 +1300

    card, cardheader, body, footer...

--- simple_chakra_dashboard/src/pages/Dashboard.tsx
+++ simple_chakra_dashboard/src/pages/Dashboard.tsx
@@ -1,6 +1,20 @@
-import { SimpleGrid, Box } from "@chakra-ui/react";
+import {
+  SimpleGrid,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Button,
+  Flex,
+  Box,
+  Heading,
+  Text,
+  HStack,
+  Divider,
+} from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { ChatIcon, ViewIcon } from "@chakra-ui/icons";
 
 interface Cards {
   id: number;
@@ -19,12 +33,50 @@ const Dashboard = () => {
   }, []);
 
   return (
-    <SimpleGrid p="10px" spacing="10px" minChildWidth="250px">
-      {cards.map((card) => (
-        <Box key={card.id} bg={"white"} h="200px" border="1px solid">
-          {card.id}
-        </Box>
-      ))}
+    <SimpleGrid p="10px" spacing="20px" minChildWidth="300px" mt={"40px"}>
+      {cards &&
+        cards.map((card) => (
+          <Card
+            key={card.id}
+            borderTop="8px"
+            borderColor={"purple.400"}
+            bg={"white"}
+          >
+            <CardHeader>
+              <Flex gap={10}>
+                <Box w="50px" h="50px">
+                  AV
+                </Box>
+                <Box>
+                  <Heading as="h3" size="sm">
+                    {card.title}
+                  </Heading>
+                  <Text ml="10px">by {card.author}</Text>
+                </Box>
+              </Flex>
+            </CardHeader>
+            <CardBody color={"gray.500"}>{card.description}</CardBody>
+            <Divider color={"gray.200"} />
+            <CardFooter>
+              <HStack>
+                <Button
+                  variant="outline"
+                  leftIcon={<ViewIcon />}
+                  colorScheme="purple"
+                >
+                  Watch
+                </Button>
+                <Button
+                  variant="solid"
+                  leftIcon={<ChatIcon />}
+                  colorScheme="purple"
+                >
+                  Comment
+                </Button>
+              </HStack>
+            </CardFooter>
+          </Card>
+        ))}
     </SimpleGrid>
   );
 };
