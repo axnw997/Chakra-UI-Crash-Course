eee90320e01dbeff237a8a62b26640164970922d
Ellie Wang <axnw997@aucklanduni.ac.nz>
2024-01-17 18:06:18 +1300

    successfully connected to backend using json-server & axios

--- simple_chakra_dashboard/src/pages/Dashboard.tsx
+++ simple_chakra_dashboard/src/pages/Dashboard.tsx
@@ -1,27 +1,30 @@
 import { SimpleGrid, Box } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+interface Cards {
+  id: number;
+  title: string;
+  description: string;
+  author: string;
+  img: string;
+}
 
 const Dashboard = () => {
+  const [cards, setCards] = useState<Cards[]>([]);
+  useEffect(() => {
+    axios.get("http://localhost:3004/tasks").then((res) => {
+      setCards(res.data);
+    });
+  }, []);
+
   return (
     <SimpleGrid p="10px" spacing="10px" minChildWidth="250px">
-      <Box bg={"white"} h="200px" border="1px solid"></Box>
-      <Box bg={"white"} h="200px" border="1px solid"></Box>
-      <Box bg={"white"} h="200px" border="1px solid"></Box>
-      <Box bg={"white"} h="200px" border="1px solid"></Box>
-
-      <Box bg={"white"} h="200px" border="1px solid"></Box>
-      <Box bg={"white"} h="200px" border="1px solid"></Box>
-      <Box bg={"white"} h="200px" border="1px solid"></Box>
-      <Box bg={"white"} h="200px" border="1px solid"></Box>
-
-      <Box bg={"white"} h="200px" border="1px solid"></Box>
-      <Box bg={"white"} h="200px" border="1px solid"></Box>
-      <Box bg={"white"} h="200px" border="1px solid"></Box>
-      <Box bg={"white"} h="200px" border="1px solid"></Box>
-
-      <Box bg={"white"} h="200px" border="1px solid"></Box>
-      <Box bg={"white"} h="200px" border="1px solid"></Box>
-      <Box bg={"white"} h="200px" border="1px solid"></Box>
-      <Box bg={"white"} h="200px" border="1px solid"></Box>
+      {cards.map((card) => (
+        <Box key={card.id} bg={"white"} h="200px" border="1px solid">
+          {card.id}
+        </Box>
+      ))}
     </SimpleGrid>
   );
 };
